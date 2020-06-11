0cbfb8b492bab9c4b0031223d9aaf8a0e8cedef2
maoyining <2044964754@qq.com>
2020-06-11 14:57:20 +0800

    项目初始化

---
+++ my-project/src/pages/home/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont">&#xe637;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .header
+    display:flex
+    line-height:.86rem
+    background: $bgColor
+    color:#fff
+    .header-left
+      width: 0.64rem
+      float:left
+      text-align :center
+    .header-input
+      flex:1
+      background :#fff
+      border-radius:.1rem
+      line-height:.64rem
+      height:.64rem
+      margin-top:.12rem
+      margin-left:.2rem
+      color:#ccc
+      text-align :center
+    .header-right
+      width:1.24rem
+      float:right
+      text-align :center
+</style>
---
+++ my-project/src/pages/list/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    list
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
